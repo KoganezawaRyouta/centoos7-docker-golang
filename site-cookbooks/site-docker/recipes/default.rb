8c41cacbac5389fcab53a9aa74295ee35513db5c
koganezawa <koganezawa@enigmo.co.jp>
2016-01-30 18:19:53 +0900

    add recipe for docker-compose

--- site-cookbooks/site-docker/recipes/default.rb
+++ site-cookbooks/site-docker/recipes/default.rb
@@ -6,6 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package "docker" do
+package 'docker' do
   action :install
+end
+
+bash 'install docker-compose' do
+  user 'root'
+  group 'root'
+  code <<-EOH
+    curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+  EOH
+  not_if 'test -f /usr/local/bin/docker-compose'
 end
